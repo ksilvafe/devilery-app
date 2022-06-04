15f20c203a4bb482278dddb87f305b8266be4d30
ksilvafe <ferreirakevin325@gmail.com>
2022-06-04 06:10:52 -0300

    create componente banner

---
+++ components/Banner/index.tsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+export const Banner = () => {
+    return (
+        <div className={styles.container}>
+           <Swiper
+                slidesPerView={1}
+                >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+            </Swiper>
+        </div>
+    );
+}

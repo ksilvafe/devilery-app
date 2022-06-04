39a82848eb7dd42e0771c21febebd88b066a1433
ksilvafe <ferreirakevin325@gmail.com>
2022-06-04 06:11:09 -0300

    import banner

--- pages/[tenant]/index.tsx
+++ pages/[tenant]/index.tsx
@@ -1,3 +1,4 @@
+import { Banner } from '../../components/Banner';
 import { SearchInput } from '../../components/SearchInput';
 import styles from '../../styles/Home.module.css';
 
@@ -29,6 +30,7 @@ const Home = () => {
             />
           </div>
         </header>
+        <Banner />
     </div>
   );
 }

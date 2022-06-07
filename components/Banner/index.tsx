69748328f3195e9171a4d70d2f771b5104848f1b
ksilvafe <ferreirakevin325@gmail.com>
2022-06-06 23:56:30 -0300

    finished banner component

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,15 +1,21 @@
-import styles from './styles.module.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+
+import styles from './styles.module.css';
+import { Autoplay } from 'swiper';
 
 export const Banner = () => {
     return (
         <div className={styles.container}>
            <Swiper
                 slidesPerView={1}
+                loop={true}
+                autoplay={{delay: 1000, disableOnInteraction: false}}
+                modules={[Autoplay]}
+                className={styles.swiper}
                 >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide className={styles.slide}><div className={styles.slideImg}>1</div></SwiperSlide>
+                <SwiperSlide className={styles.slide}><div className={styles.slideImg}>2</div></SwiperSlide>
             </Swiper>
         </div>
     );

20268febef8179ca183335f42e8257f434eeb26e
ksilvafe <ferreirakevin325@gmail.com>
2022-06-07 00:06:15 -0300

    Create component Product Item

---
+++ components/ProductItem/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.css';
+
+export const ProductItem = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.head}></div>
+            <div className={styles.info}></div>
+        </div>
+    );
+}
--- pages/[tenant]/index.tsx
+++ pages/[tenant]/index.tsx
@@ -1,4 +1,5 @@
 import { Banner } from '../../components/Banner';
+import { ProductItem } from '../../components/ProductItem';
 import { SearchInput } from '../../components/SearchInput';
 import styles from '../../styles/Home.module.css';
 
@@ -31,6 +32,12 @@ const Home = () => {
           </div>
         </header>
         <Banner />
+
+        <div className={styles.grid}>
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+        </div>
     </div>
   );
 }
